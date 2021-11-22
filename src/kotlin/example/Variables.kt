aa28826a7540f9638ef75043bdf18b6156dd019a
YoungWukJeon <kdh10080@gmail.com>
2021-11-23 01:29:13 +0900

    Variables

---
+++ src/kotlin/example/Variables.kt
@@ -0,0 +1,27 @@
+package example
+
+fun main() {
+    var a: String = "initial"
+    println(a)
+
+    val b: Int = 1
+    val c = 3
+
+//    var e: Int
+//    println(e) // Compile Error Occurred
+
+    val d: Int
+
+    if (someCondition()) {
+        d = 1
+    } else {
+        d = 2
+    }
+
+    println(d)
+}
+
+fun someCondition(): Boolean {
+    return true
+}
+
