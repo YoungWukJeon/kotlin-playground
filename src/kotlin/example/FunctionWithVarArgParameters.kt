9b41deaccae6c76ac6cb45b8ad756e028be3d4c5
YoungWukJeon <kdh10080@gmail.com>
2021-11-23 01:21:24 +0900

    Functions

---
+++ src/kotlin/example/FunctionWithVarArgParameters.kt
@@ -0,0 +1,20 @@
+package example
+
+fun main() {
+    fun printAll(vararg messages: String) {
+        for (m in messages) println(m)
+    }
+    printAll("Hello", "Hallo", "Halo", "你好")
+
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {
+        for (m in messages) println(prefix + m)
+    }
+    printAllWithPrefix(
+        "Hello", "Hallo", "Salut", "Halo", "你好",
+        prefix = "Greeting: "
+    )
+
+    fun log(vararg entries: String) {
+        printAll(*entries)
+    }
+}
